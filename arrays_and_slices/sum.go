505aa5f23a375355bb3e6b9fc3ff8202b9a19fe0
amagkn <dmitrii.amagkn@gmail.com>
2024-10-12 22:14:10 +0300

    Arrays and slices (part 2)

--- arrays_and_slices/sum.go
+++ arrays_and_slices/sum.go
@@ -9,3 +9,31 @@ func Sum(numbers []int) int {
 
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		var tail []int
+
+		if len(numbers) == 0 {
+			tail = []int{}
+		} else {
+			tail = numbers[1:]
+		}
+
+		sums = append(sums, Sum(tail))
+	}
+
+	return sums
+}
